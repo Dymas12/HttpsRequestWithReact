8bd98f5a3d61c268ef15682c2afe55ad676fce25
Ruan Nunes <ruan.nunes@lovyca.com>
2022-10-21 17:35:28 -0300

    Custom hooks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 
 import './App.css';
 import { useState , useEffect } from 'react';
+import {useFetch} from './hooks/useFetch'
+const url  = "http://localhost:3000/products"
 
 function App() {
   const [products , setProducts] = useState([]);
+
+  // 4 - custom hook
+
+
+  const {data:items} = useFetch(url);
+
   const [name,setName] = useState("");
   const [price , setPrice] = useState("");
 
-  const url  = "http://localhost:3000/products"
 
-  useEffect(()=>{
-    async function fetchMyApi(){
-      const res = await fetch(url);
 
-      const data = await res.json()
-  
-      setProducts(data)
-    }
-    fetchMyApi()
-  },[]);
+  //useEffect(()=>{
+  //  async function fetchMyApi(){
+  //    const res = await fetch(url);
+//
+  //    const data = await res.json()
+  //
+  //    setProducts(data)
+  //  }
+  //  fetchMyApi()
+  //},[]);
 
   const handleSubmit = async (e)=>{
     e.preventDefault();
@@ -48,7 +56,7 @@ function App() {
     <div className="App">
     <h1> Lista de Produtos</h1>
     <ul>
-      {products.map((product)=>(
+      { items && items.map((product)=>(
         <li key={product.id}>{product.name} - R$:{product.price}</li>
       ))}
     </ul>
