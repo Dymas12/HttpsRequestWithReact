8e7616beab03747cf256b48078de5ef9e0523a75
Ruan Nunes <ruan.nunes@lovyca.com>
2022-10-21 16:03:51 -0300

    Post na api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState , useEffect } from 'react';
 
 function App() {
+  const [products , setProducts] = useState([]);
+  const [name,setName] = useState("");
+  const [price , setPrice] = useState("");
+
+  const url  = "http://localhost:3000/products"
+
+  useEffect(()=>{
+    async function fetchMyApi(){
+      const res = await fetch(url);
+
+      const data = await res.json()
+  
+      setProducts(data)
+    }
+    fetchMyApi()
+  },[]);
+
+  const handleSubmit = async (e)=>{
+    e.preventDefault();
+
+    const product = {
+      name,
+      price,
+    };
+
+    const res = await fetch(url,{
+      method: "POST",
+      headers:{
+        "Content-Type":"application/json"
+      },
+      body: JSON.stringify(product),
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1> Lista de Produtos</h1>
+    <ul>
+      {products.map((product)=>(
+        <li key={product.id}>{product.name} - R$:{product.price}</li>
+      ))}
+    </ul>
+    <div className="add-product">
+      <form onSubmit={handleSubmit}>
+
+        <label>
+          Nome:
+          <input type="text" value={name} name="name" onChange={(e)=> setName(e.target.value)} />
+        </label>
+        <label>Preço:
+          <input type="text" value={price} price="price" onChange={(e) => setPrice(e.target.value)} />
+        </label>
+        <input type="submit"  value="Criar"/>
+      </form>
+    </div>
     </div>
   );
 }
